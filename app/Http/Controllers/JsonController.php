665f42fff9c83bb43d9376e90a55a74b74d86dc1
Mochammad Alfin Faturochman <alfinfaturochmanm@gmail.com>
2020-02-10 22:48:48 +0700

    JSON Web Token Initialization

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class IntepreterController extends Controller
+class JsonController extends Controller
 {
     /**
      * Create a new controller instance.
