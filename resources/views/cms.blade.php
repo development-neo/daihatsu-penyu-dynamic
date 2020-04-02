7f11d4d592051994c28142e8a97a985b1dd097d7
Mochammad Alfin Faturochman <alfinfaturochmanm@gmail.com>
2020-04-02 18:38:39 +0700

    Update iframe url

--- resources/views/cms.blade.php
+++ resources/views/cms.blade.php
@@ -1,5 +1,5 @@
 <html>
     <body style="margin:0px;">
-        <iframe src="http://127.0.0.1:8001" style="width:100%; height: 100%;border: none;"></iframe>
+        <iframe src="http://development.neodigital.co.id/dynamic-system/public" style="width:100%; height: 100%;border: none;"></iframe>
     </body>
 </html>
